942a6e167a738d0901a2a00931b02d1f51cb08a8
Abhik Jain <abhikjain360@gmail.com>
2021-01-30 11:18:49 +0530

    filter function generalized

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -2,19 +2,15 @@ pub mod average;
 pub mod gaussian;
 pub mod median;
 
-use na::{Dynamic, MatrixSlice};
-use nalgebra as na;
-
 use super::Df32;
 
 pub struct Filter<const N: usize> {
-    pub kernel:
-        Box<dyn Fn(MatrixSlice<'_, [f32; N], Dynamic, Dynamic, na::U1, Dynamic>) -> [f32; N]>,
     pub height: usize,
     pub width: usize,
+    pub kernel: Box<dyn Fn(&Df32<N>, (usize, usize)) -> [f32; N]>
 }
 
-pub fn conv2d<const N: usize>(filter: Filter<N>, img: &Df32<N>) -> Df32<N> {
+pub fn convolve<const N: usize>(filter: Filter<N>, img: &Df32<N>) -> Df32<N> {
     use crate::util::pad;
     let img = pad(img, filter.height, filter.width);
     let mut h = img.clone();
@@ -25,9 +21,10 @@ pub fn conv2d<const N: usize>(filter: Filter<N>, img: &Df32<N>) -> Df32<N> {
     for i in half_v..img.shape().0 - half_v {
         for j in half_h..img.shape().1 - half_h {
             h[(i, j)] =
-                (filter.kernel)(img.index((i - half_v..i + half_v + 1, j - half_h..j + half_h + 1)));
+                (filter.kernel)(&img, (i, j));
         }
     }
 
     h
 }
+
