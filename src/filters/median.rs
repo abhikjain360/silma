d2868b031c349419a393b465f59b125d4dca2b8b
Abhik Jain <abhikjain360@gmail.com>
2021-01-30 12:39:16 +0530

    median filter

--- src/filters/median.rs
+++ src/filters/median.rs
@@ -1,18 +1,6 @@
 use super::{convolve, Df32, Filter};
 
 pub fn median_filter<const N: usize>(mut height: usize, mut width: usize) -> Filter<N> {
-    Filter {
-        height,
-        width,
-        kernel: Box::new(move |m, idx| {
-            let mut x = [0f32; N];
-            let size = (height * width) as f32;
-
-        })
-    }
-}
-
-pub fn triangular_filter<const N: usize>(mut height: usize, mut width: usize) -> Filter<N> {
     // make height width odd, else they create problems
     if height % 2 == 0 {
         height -= 1;
@@ -24,17 +12,28 @@ pub fn triangular_filter<const N: usize>(mut height: usize, mut width: usize) ->
     Filter {
         height,
         width,
-        kernel: Box::new(move |m| {
+        kernel: Box::new(move |m, idx| {
             let mut x = [0f32; N];
-            let size = (height * width) as f32;
+            let half_v = height / 2;
+            let half_h = width / 2;
+            let size = height * width;
+            let mut v;
             for i in 0..N {
-                x[i] = m.iter().map(|elem| elem[0]).sum::<f32>() / size;
+                v = m
+                    .index((
+                        idx.0 - half_h..idx.0 + half_h + 1,
+                        idx.1 - half_v..idx.1 + half_v + 1,
+                    ))
+                    .iter()
+                    .map(|elem| elem[i] as u8)
+                    .collect::<Vec<u8>>();
+                v.sort_unstable();
+                x[i] = v[size / 2] as f32;
             }
             x
         }),
     }
 }
-
-pub fn average<const N: usize>(img: &Df32<N>, kernel_size: usize) -> Df32<N> {
-    convolve(triangular_filter(kernel_size, kernel_size), img)
+pub fn median<const N: usize>(img: &Df32<N>, kernel_size: usize) -> Df32<N> {
+    convolve(median_filter(kernel_size, kernel_size), img)
 }
